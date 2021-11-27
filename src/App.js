bfa516f98374ff92f356d2ba6a557111e7a75da1
Riley-Hoffman <hoffman.riley@gmail.com>
2021-11-27 15:19:39 -0500

    added pseudo code and adjusted indent/formatting.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import axios from 'axios';
-import {useEffect} from 'react';
+// import axios from 'axios';
+// import {useEffect} from 'react';
 import Search from './components/Search';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -26,42 +26,9 @@ import Footer from './components/Footer';
 
 
 function App() {
-    useEffect(() => {
-      // const apiKeyMov = `786c1383f2a24f7ee0f7ae525d2a9af4`
-      // const apiKeyGif = `gaER4NoH8CUjV7xNcUlFZdhPlYFRc1EY`
-      // axios({
-      //   url: "https://api.themoviedb.org/3/search/movie",
-      //   params: {
-      //     api_key: apiKeyMov, 
-      //     query: `Toy Story 2`, 
-      //   }
-      // }).then((response) => {
-      //   console.log(response.data.results)
-      // })
-
-
-      // axios({
-      //   url: `https://api.themoviedb.org/3/movie/863/keywords`,
-      //   params: {
-      //     api_key: apiKeyMov 
-      //   }
-      // }).then((response) => {
-      //   console.log(response.data)
-      // })
-
-
-
-      // axios ({
-      //   url: `https://api.giphy.com/v1/gifs/search`,
-      //   params: {
-      //     api_key: apiKeyGif,
-      //     q: 'suicide'
-
-      //   }
-      // }).then((response) => {
-      //   console.log(response.data)
-      // })
-    }, [])
+    // useEffect(() => {
+      
+    // }, [])
 
   return (
     <div className="App">
--- src/components/Search.js
+++ src/components/Search.js
@@ -5,28 +5,28 @@ import axios from 'axios';
 
 
 function Search() {
+  
+  const [searchInput, setSearchInput] = useState ('') 
+  // const [keyword] = useState ('')
 
-    const [searchInput, setSearchInput] = useState ('') 
-    // const [keyword] = useState ('')
-
-    const handleSearchInput = (e) => {
+  const handleSearchInput = (e) => {
     setSearchInput(e.target.value);
-    }
+  }
 
-    const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
+    
     const apiKeyMov = `786c1383f2a24f7ee0f7ae525d2a9af4`
     //Call API by inputing movie title 
     axios({
-        url: "https://api.themoviedb.org/3/search/movie",
-        params: {
-          api_key: apiKeyMov, 
-          query: searchInput, 
-        }
+      url: "https://api.themoviedb.org/3/search/movie",
+      params: {
+        api_key: apiKeyMov, 
+        query: searchInput, 
+      }
       }).then((response) => {
         console.log(response.data.results)
-
+    
         const movieId = response.data.results[0].id
         //Pass movie id to keyword point
         axios({
@@ -34,56 +34,62 @@ function Search() {
         params: {
           api_key: apiKeyMov 
         }
-      }).then((response) => {
-        console.log(response.data)
-        const keywordArray = []
-        for(let keyName in response.data.keywords){
-            keywordArray.push(response.data.keywords[keyName].name)
-        }
-        console.log(keywordArray)
-        for (let i = keywordArray.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [keywordArray[i], keywordArray[j]] = [keywordArray[j], keywordArray[i]];
-        }
-        console.log(keywordArray)
-        const threeKeywordArray = keywordArray.slice(0,3)
-        console.log(threeKeywordArray)
-        return threeKeywordArray
-      })
-      .then((response) => {
-        console.log(response) 
-        response.forEach(keyword => {
-            const apiKey ='vKgSlbA9IvP9mzh808UAXFD7YeIabsQe'
-    axios({
-        url: 'https://api.giphy.com/v1/gifs/search', 
-        params: {
-            api_key: apiKey,
-            q: keyword, 
-        }
-    }).then((response) => {
-        const array = response.data.data
-         const random = array[Math.floor(Math.random() * array.length)]
-         console.log(random)
-        })
-        })
-      }
-      )
-      }) 
-      setSearchInput('')
-    }
-    
-    return (
-        <section className="searchSection" id="searchSection">
-            <div className="searchWrapper wrapper">
-                <h2>Lorem ipsum dolor sit amet.</h2>
-                <form action="submit" onSubmit={handleSubmit} role="search">
-                    <label htmlFor="search">Lorem ipsum dolor sit amet consectetur adipisicing.</label>
-                    <input type="search" id="search" placeholder="Enter a movie" value={searchInput} onChange={handleSearchInput} required/>
-                    <button>Submit</button>
-                </form>
-            </div>
-        </section>    
-    )
+        }).then((response) => {
+          console.log(response.data)
+          // An array to hold keywords
+          const keywordArray = []
+          for (let keyName in response.data.keywords) {
+              keywordArray.push(response.data.keywords[keyName].name)
+          }
+          console.log(keywordArray)
+          // Randomize keywords
+          for (let i = keywordArray.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [keywordArray[i], keywordArray[j]] = [keywordArray[j], keywordArray[i]];
+          }
+          console.log(keywordArray)
+          // Take only three keywords
+          const threeKeywordArray = keywordArray.slice(0,3)
+          console.log(threeKeywordArray)
+          return threeKeywordArray
+          })
+          .then((response) => {
+            console.log(response) 
+            // Pass each keyword to GIPHY API
+            response.forEach(keyword => {
+                const apiKey ='vKgSlbA9IvP9mzh808UAXFD7YeIabsQe'
+            axios({
+                url: 'https://api.giphy.com/v1/gifs/search', 
+                params: {
+                    api_key: apiKey,
+                    q: keyword, 
+                }
+              }).then((response) => {
+                  const array = response.data.data
+                  // Randomize GIF results
+                  const random = array[Math.floor(Math.random() * array.length)]
+                  console.log(random)
+                })
+              })
+            }
+          )
+        }) 
+    // Clear search input field 
+    setSearchInput('')
+  }
+  
+  return (
+      <section className="searchSection" id="searchSection">
+          <div className="searchWrapper wrapper">
+              <h2>Lorem ipsum dolor sit amet.</h2>
+              <form action="submit" onSubmit={handleSubmit} role="search">
+                  <label htmlFor="search">Lorem ipsum dolor sit amet consectetur adipisicing.</label>
+                  <input type="search" id="search" placeholder="Enter a movie" value={searchInput} onChange={handleSearchInput} required/>
+                  <button>Submit</button>
+              </form>
+          </div>
+      </section>    
+  )
 }
 
 export default Search
