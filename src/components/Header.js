7693c99177d58b6469c5701593c6503d540186d4
Riley-Hoffman <hoffman.riley@gmail.com>
2021-11-28 09:50:30 -0500

    added skip-to-content button and general focus styles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 function Header() {
     return (
         <header className="flexBox">
+            {/* hidden/tab focusable skip to content button */}
+            <a href="#searchSection" class="skip-link">Skip To Content</a>
             <div className="wrapper">
                 <div className="flexBox">
                     {/* popcorn image */}
@@ -11,7 +13,7 @@ function Header() {
                     {/* film strip image */}
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quidem sunt ullam, molestiae optio nemo mollitia ad sit tempora dicta!</p>
                     {/* moved this link into the div for positioning */}
-                    <a href="#searchSection">Find GIF!</a> 
+                    <a href="#searchSection" class="button">Find GIF!</a> 
                 </div>
             </div>
         </header>
